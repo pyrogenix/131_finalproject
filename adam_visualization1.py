fca7dbe0d5931ca4af96c57453e344c9a6d1e91b
pyrogenix <mach1amh@gmail.comgit config --global user.email mach1amh@gmail.com>
2021-11-21 15:44:35 -0700

    worked on visualizations

--- adam_visualization1.py
+++ adam_visualization1.py
@@ -1,11 +1,27 @@
+"""
+    Adam Heitzman
+
+    ISTA 131 Final Project
+
+    Visualization code #1. ...
+"""
+
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-csv = pd.read_csv('./datasets/USvideos.csv')
+def setup():
+    csv = pd.read_csv('./datasets/USvideos.csv')
 
 def makeplot():
     plt.plot(csv.views, csv.likes)
 
 def show():
     plt.show()
+
+def main():
+    setup()
+
+
+if __name__ == "__main__":
+    main()
