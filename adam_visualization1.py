bf44e689ddc4dfc7ba9b2cb3810c6f8b5be63e5e
pyrogenix <mach1amh@gmail.comgit config --global user.email mach1amh@gmail.com>
2021-11-30 11:30:13 -0700

    overhauled vis 1

--- adam_visualization1.py
+++ adam_visualization1.py
@@ -3,9 +3,9 @@
 
     ISTA 131 Final Project
 
-    Visualization code #1. Plots number of likes vs number of comments on a chart
-    and shows regression line. Interesting to note how the like/comment ratio follows
-    a number of distinct lines, as opposed to being randomly scattered.
+    Visualization code #1. Plots number of view vs number of likes and dislikes on respective
+    subplots, also showing regression lines. Interesting to note how the like/comment ratio follows
+    a number of distinct trend lines, as opposed to being randomly scattered.
 """
 
 import pandas as pd
@@ -14,27 +14,55 @@ import statsmodels.api as sm
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+"""
+    Open CSV and calls function to make subplots
+"""
 def setup():
     csv = pd.read_csv('./datasets/USvideos.csv')
-    makeplot(csv)
+    make_subplots(csv)
 
-def makeplot(csv):
-    x = csv.comment_count
+"""
+    Makes subplots to compare view count vs likes and dislikes, showing a regression line for each.
+    Initializes plots, sets custom axes and labels, saves the image, and shows the final output.
+"""
+def make_subplots(csv):
+    x = csv.views
     y = csv.likes
-    sns.set(rc={"figure.figsize":(8, 4)})
-    plot = sns.regplot(x, y, data=csv)
-    plot.set_xlabel('Number of Comments (in millions)', fontsize=12)
-    plot.set_ylabel('Number of Likes (in millions)', fontsize=12)
-    plot.set_title('Likes vs Comments', fontsize=15, weight=600)
-    plt.xticks([0, 200000, 400000, 600000, 800000, 1000000, 1200000, 1400000],[0, .2, .4, .6, .8, 1, 1.2, 1.4])
-    plt.yticks([0, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000],[0, 1, 2, 3, 4, 5, 6, 7])
+    z = csv.dislikes
+
+    f = plt.figure(figsize=(12, 8))
+    gs = f.add_gridspec(2, 1)
+
+    with sns.axes_style("darkgrid"):
+        ax = f.add_subplot(gs[0, 0])
+        plot = sns.regplot(x, y, data=csv)
+        plot.set_xlabel('Views (in millions)', fontsize=12)
+        plot.set_ylabel('Likes (in millions)', fontsize=12)
+        plot.set_title('Views vs Likes', fontsize=15, weight=600)
+        plt.xticks([0, 20000000, 40000000, 60000000, 80000000, 100000000, 120000000, 140000000, 160000000, 180000000, 200000000, 220000000, 240000000],\
+            [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240])
+        plt.yticks([0, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000],[0, 1, 2, 3, 4, 5, 6, 7])
+
+    with sns.axes_style("darkgrid"):
+        ax = f.add_subplot(gs[1, 0])
+        plot2 = sns.regplot(x, z, data=csv)
+        plot2.set_xlabel('Views (in millions)', fontsize=12)
+        plot2.set_ylabel('Dislikes (in millions)', fontsize=12)
+        plot2.set_title('Views vs Dislikes', fontsize=15, weight=600)
+        plt.xticks([0, 20000000, 40000000, 60000000, 80000000, 100000000, 120000000, 140000000, 160000000, 180000000, 200000000, 220000000, 240000000],\
+            [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240])
+        plt.yticks([0, 200000, 400000, 600000, 800000, 1000000, 1200000, 1400000, 1600000, 1800000, 2000000],[0, .2, .4, .6 , .8, 1, 1.2, 1.4, 1.6, 1.8, 2])
+    f.tight_layout()
+
     plt.savefig('ADAM_VIS1.png', bbox_inches="tight")
     print('-----')
     print('Figure has been saved as ADAM_VIS1.png')
     print('-----')
     plt.show()
 
-
+"""
+    Automatically calls setup when program starts
+"""
 def main():
     setup()
 
