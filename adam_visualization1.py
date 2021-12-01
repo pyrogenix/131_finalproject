5c92d73e5c5d130e6cce31b448ac89952b4c6e48
pyrogenix <mach1amh@gmail.comgit config --global user.email mach1amh@gmail.com>
2021-11-30 21:33:46 -0700

    fixed error message and updated documentation

--- adam_visualization1.py
+++ adam_visualization1.py
@@ -3,9 +3,10 @@
 
     ISTA 131 Final Project
 
-    Visualization code #1. Plots number of view vs number of likes and dislikes on respective
-    subplots, also showing regression lines. Interesting to note how the like/comment ratio follows
-    a number of distinct trend lines, as opposed to being randomly scattered.
+    Visualization code #1. Mode 1 plots number of view vs number of likes and dislikes on respective
+    subplots, also showing regression lines. Mode 2 does the same but overlapping on one plot.Interesting 
+    to note how the like/comment ratio follows a number of distinct trend lines, as opposed to points being 
+    randomly scattered.
 """
 
 import pandas as pd
@@ -15,7 +16,10 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 
 """
-    Open CSV and calls function to make subplots
+    Open CSV and calls user input to select mode.
+    Enter 1 to display views vs likes and views vs dislikes as two separate subplots.
+    Enter 2 to display views vs likes and dislikes overlapping on one plot.
+    Will call appropriate functions following input.
 """
 def setup():
     csv = pd.read_csv('./datasets/USvideos.csv')
@@ -42,9 +46,10 @@ def make_subplots(csv):
     f = plt.figure(figsize=(12, 8))
     gs = f.add_gridspec(2, 1)
 
+    # Makes views vs likes subplot using Seaborn's darkgrid style.
     with sns.axes_style("darkgrid"):
         ax = f.add_subplot(gs[0, 0])
-        plot = sns.regplot(x, y, data=csv, scatter_kws={'s':4}, line_kws={"color": "red"})
+        plot = sns.regplot(x=x, y=y, data=csv, scatter_kws={'s':4}, line_kws={"color": "red"})
         plot.set_xlabel('Views (in millions)', fontsize=12)
         plot.set_ylabel('Likes (in millions)', fontsize=12)
         plot.set_title('Views vs Likes', fontsize=15, weight=600)
@@ -53,9 +58,10 @@ def make_subplots(csv):
         plt.yticks([0, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000],\
             [0, 1, 2, 3, 4, 5, 6])
 
+    # Makes views vs dislikes subplot using Seaborn's darkgrid style.
     with sns.axes_style("darkgrid"):
         ax = f.add_subplot(gs[1, 0])
-        plot2 = sns.regplot(x, z, data=csv, scatter_kws={'s':4}, line_kws={"color": "red"})
+        plot2 = sns.regplot(x=x, y=z, data=csv, scatter_kws={'s':4}, line_kws={"color": "red"})
         plot2.set_xlabel('Views (in millions)', fontsize=12)
         plot2.set_ylabel('Dislikes (in millions)', fontsize=12)
         plot2.set_title('Views vs Dislikes', fontsize=15, weight=600)
@@ -71,10 +77,19 @@ def make_subplots(csv):
     print('-----')
     plt.show()
 
+"""
+    Plots views vs likes AND dislikes on one plot where the latter overlap.
+    Initializes plot, sets custom axes and labels, saves the image, and shows the final output.
+"""
 def make_plot(csv):
+    x = csv.views
+    y = csv.likes
+    z = csv.dislikes
+
+    # Makes views vs likes and dislikes regplot using Seaborn's darkgrid style.
     with sns.axes_style("darkgrid"):
-        plot = sns.regplot(csv.views, csv.likes, data=csv, scatter_kws={'s':2})
-        plot2 = sns.regplot(csv.views, csv.dislikes, data=csv, scatter_kws={'s':2})
+        plot = sns.regplot(x=x, y=y, data=csv, scatter_kws={'s':2})
+        plot2 = sns.regplot(x=x, y=z, data=csv, scatter_kws={'s':2})
         plot.set_xlabel('Views (in millions)', fontsize=12)
         plot.set_ylabel('Likes / Dislikes (in millions)', fontsize=12)
         plot.set_title('Views vs Likes / Dislikes', fontsize=15, weight=600)
@@ -83,6 +98,7 @@ def make_plot(csv):
         plt.yticks([0, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000],\
             [0, 1, 2, 3, 4, 5, 6])
         plt.legend(labels=["Likes","Dislikes"])
+
     plt.savefig('ADAM_VIS1-ALT.png', bbox_inches="tight", dpi=600)
     print('-----')
     print('Figure has been saved as ADAM_VIS1-ALT.png')
